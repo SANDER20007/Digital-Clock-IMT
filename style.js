aef637d7c866e9329ebe465a066636aad2005fac
SANDER20007 <salia008@osloskolen.no>
2025-12-08 08:39:06 +0100

    ddd

--- style.js
+++ style.js
@@ -1,17 +1,4 @@
-function startTime (){
-const today = new Date();
-let hours = today.getHours();
-let munits = today.getMinutes();
-let seconds = today.getSeconds ();
-munits = checkTime(munits);
- seconds = checkTime(seconds);
-  document.getElementById('Hours').innerHTML =  hours ;
-  document.getElementById('Minutes').innerHTML =  munits ;
-   document.getElementById('Seconds').innerHTML =  seconds ;
-  setTimeout(startTime, 1000);
+FontFacethfgæ
 
-}
-function checkTime(i) {
-  if (i < 10) {i = "0" + i};  
-  return i;
-}
+
+
